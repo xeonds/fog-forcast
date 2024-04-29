faad9b94c329c6557bcc233af5117681d34e92c9
xeonds <xeonds@stu.xidian.edu.cn>
2024-04-29 23:35:45 +0800

    feat: add locator for adding city

--- app/lib/views/city.dart
+++ app/lib/views/city.dart
@@ -3,6 +3,7 @@ import 'package:app/main.dart';
 import 'package:app/service/weather_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
@@ -79,33 +80,57 @@ class MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick a Position'),
-      ),
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(34, 108), // Initial map center
-          zoom: 5.0, // Initial zoom level
-          onTap: _handleTap, // Handle tap event
+        appBar: AppBar(
+          title: const Text('Pick a Position'),
         ),
-        mapController: MapController(),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+        body: FlutterMap(
+          options: MapOptions(
+            center: LatLng(34, 108), // Initial map center
+            zoom: 5.0, // Initial zoom level
+            onTap: _handleTap, // Handle tap event
           ),
-          MarkerLayer(
-            markers: [selectedMarker],
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context, selectedPosition);
-        },
-        child: const Icon(Icons.check),
-      ),
-    );
+          mapController: MapController(),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+            ),
+            MarkerLayer(
+              markers: [selectedMarker],
+            ),
+          ],
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () async {
+                Position position = await Geolocator.getCurrentPosition(
+                    desiredAccuracy: LocationAccuracy.high);
+                setState(() {
+                  selectedPosition =
+                      LatLng(position.latitude, position.longitude);
+                  selectedMarker = Marker(
+                    point: selectedPosition,
+                    builder: (context) => const Icon(
+                      Icons.location_on,
+                      size: 50,
+                      color: Colors.red,
+                    ),
+                  );
+                });
+              },
+              child: const Icon(Icons.gps_fixed),
+            ),
+            const SizedBox(height: 16.0),
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.pop(context, selectedPosition);
+              },
+              child: const Icon(Icons.check),
+            ),
+          ],
+        ));
   }
 
   // Handle tap event on the map
